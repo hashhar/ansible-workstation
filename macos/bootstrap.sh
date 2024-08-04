5c6d386c71af4719bb4f35b479c6dacafd69e17c
Ashhar Hasan <hashhar_dev@outlook.com>
2024-08-04 19:49:40 +0530

    even more working stuff

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -14,12 +14,7 @@ ansible-galaxy install -r requirements.yml
 ansible-playbook -i inventory -K main.yml
 
 # Change user's SHELL to brew managed zsh
-#sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
-
-# Enable docker completion
-#etc=/Applications/Docker.app/Contents/Resources/etc
-#ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-#ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
+sudo dscl . -create /Users/$USER UserShell /opt/homebrew/bin/zsh
 
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "argon"
