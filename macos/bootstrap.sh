5451fa21e4f0b94e0d306cc70af3a141c38c9fbc
Ashhar Hasan <hashhar_dev@outlook.com>
2024-09-13 12:29:37 +0530

    Install python only if not available

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-brew install python
+if ! command -v python &>/dev/null; then
+    brew install python
+fi
 python3 -m venv venv
 . venv/bin/activate
 pip install ansible paramiko
